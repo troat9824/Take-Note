f99afc06a6c0d098cafff47005030dbec12c26ca
troat9824 <troat9824@gmail.com>
2022-04-27 18:05:55 -0500

    get request works

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,17 +1,16 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const { showNotes, createNewNote, validateNote } = require('../../lib/notes');
 // desctructures note to access the array inside the JSON object
-const { notesArray } = require('../../db/db.json');
+const { notesArray } = require('../../db/db');
 
 // Get all notes
 router.get('/notes', (req, res) => {
-    let results = notesArray;
-    if (req.query) {
-      results = showNotes(req.query, results);
-    }
-    res.json(results);
-});
+  
+       res.json(showNotes(notesArray));
+   }
+   );
 
 // Add note
 router.post('/notes', (req, res) => {
