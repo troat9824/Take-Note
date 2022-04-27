11ed7dd589d18498b5bfa017b0b34725633c819f
troat9824 <troat9824@gmail.com>
2022-04-26 21:49:32 -0500

    htmlRoutes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -3,5 +3,18 @@ const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
-    
-});
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+  
+  router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+  
